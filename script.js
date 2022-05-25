4d6927485f43934cd7abd23899fe5c5fd23eb555
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-05-25 16:36:51 +0800

    Password Message
    A message indicating matching or mismatch of password

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var check = function() {
     if (document.getElementById('password1').value ==
       document.getElementById('password2').value) {
       document.getElementById('password2').style.borderColor = 'green';
-      document.getElementById('password2').innerHTML = 'matching';
+      document.getElementById('message').innerHTML = 'Matched';
     } else {
       document.getElementById('password2').style.borderColor = 'red';
-      document.getElementById('password2').innerHTML = 'not matching';
+      document.getElementById('message').innerHTML = '*Not Matching';
+      document.getElementById('message').style.color = 'red';
     }
   }
 
